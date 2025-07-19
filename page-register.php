5901840cf575692879d082632bf643918ef24dad
AdelmoI <138867051+AdelmoI@users.noreply.github.com>
2025-07-19 12:24:52 +0200

    Update page-register.php

--- page-register.php
+++ page-register.php
@@ -72,10 +72,10 @@ get_header(); ?>
                             
                             wp_set_current_user($user_id);
                             wp_set_auth_cookie($user_id);
+                            do_action('wp_login', $email, get_user_by('ID', $user_id));
                             
                             $success = true;
-                            echo '<script>setTimeout(function(){ window.location.href = "' . home_url('/profilo') . '"; }, 1000);</script>';
-                        } else {
+                            echo '<script>setTimeout(function(){ window.location.reload(); setTimeout(function(){ window.location.href = "' . home_url('/profilo') . '"; }, 500); }, 1000);</script>';                        } else {
                             $errors[] = 'Errore durante la registrazione. Riprova.';
                         }
                     }
